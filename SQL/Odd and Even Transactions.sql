77f45c921d6fa115abe1e02a92a03447a4e3107e
umerfaisall <faisalumer445@gmail.com>
2026-02-08 17:52:49 +0500

    feat: Add SQL query to calculate odd and even transaction sums by date.

---
+++ SQL/Odd and Even Transactions.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT transaction_date,sum(if(amount%2 != 0 ,amount,0)) as odd_sum, sum(if(amount%2 = 0 ,amount,0)) as even_sum
+from transactions
+group by transaction_date
+order by transaction_date asc
