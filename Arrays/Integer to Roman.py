21e143e64928dbcc916bee238c4142922726a8b0
umerfaisall <faisalumer445@gmail.com>
2026-02-11 01:25:33 +0500

    Solved Integer to Roman

---
+++ Arrays/Integer to Roman.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        v_dict = {
+            'M': 1000,
+            'CM': 900,
+            'D': 500,
+            'CD': 400,
+            'C': 100,
+            'XC': 90,
+            'L': 50,
+            'XL': 40,
+            'X': 10,
+            'IX': 9,
+            'V': 5,
+            'IV': 4,
+            'I': 1,
+        }
+        ans = ''
+        for key, value in v_dict.items():
+            count = num//value
+            ans+=count*key
+            num-=count*value
+        return ans
